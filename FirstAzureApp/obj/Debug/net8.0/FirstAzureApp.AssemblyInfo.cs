a9eab5d0360c64513306186d7b01210f677135bf
Koushal <koushalmshastry15@gmail.com>
2024-05-06 13:01:55 +0530

    changed at 13:01

--- FirstAzureApp/obj/Debug/net8.0/FirstAzureApp.AssemblyInfo.cs
+++ FirstAzureApp/obj/Debug/net8.0/FirstAzureApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstAzureApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d33d9ed37b7ece685a44e9eef22a204b1b3d1d18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a45c8c4bea1c027d7a1cde54fe9e0d567f8c0111")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstAzureApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstAzureApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
